c1b250890d33e7cde0531a094e2f57ea976d199f
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-26 16:04:10 -0300

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { LogoRocket } from "./LogoRocket";
+
+export function Footer() {
+    return (
+        <footer className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+            <LogoRocket />
+        </footer>
+    )
+}
